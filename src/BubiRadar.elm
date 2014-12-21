4c28a186cef58e6f5ba9737c23b748f27ee1c135
Laszlo Pandy <laszlo.pandy@prezi.com>
2014-12-21 21:31:00 +0100

    Spaces instead of tabs.

--- src/BubiRadar.elm
+++ src/BubiRadar.elm
@@ -13,13 +13,13 @@ port stationXmlOut = getBubiData
 main = asText <~ stationXmlIn
 
 type alias StationXml = {
-		uid : String,
-		lat : String,
-		lng : String,
-		unique_name : String,
-		num_bikes : String,
-		max_bikes : String
-	}
+        uid : String,
+        lat : String,
+        lng : String,
+        unique_name : String,
+        num_bikes : String,
+        max_bikes : String
+    }
 
 type alias Location = {
 		lag : Int,
@@ -28,30 +28,30 @@ type alias Location = {
 
 type alias Uid = String
 type alias Station = {
-		uid : Uid,
-		location : Location,
-		unique_name : String,
-		name : String,
-		num_bikes : Int,
-		max_bikes : Int,
-		distance : Maybe Int
-	}
+        uid : Uid,
+        location : Location,
+        unique_name : String,
+        name : String,
+        num_bikes : Int,
+        max_bikes : Int,
+        distance : Maybe Int
+    }
 
 type alias State = {
-		stations: List Station,
-		userLocation : Maybe Location,
-		stationView : Maybe Uid,
-		updateTime : Int,
-		waitingForData : Bool
-	}
+        stations: List Station,
+        userLocation : Maybe Location,
+        stationView : Maybe Uid,
+        updateTime : Int,
+        waitingForData : Bool
+    }
 
 getBubiData : Signal (Maybe String)
 getBubiData =
-	let url = "https://nextbike.net/maps/nextbike-live.xml?domains=mb"
-		handleResp resp =
-			case resp of
-				Http.Success data -> Just data
-				Http.Waiting -> Nothing
-				Http.Failure _ _ -> Nothing
-	in
-		handleResp <~ Http.sendGet (constant url)
+    let url = "https://nextbike.net/maps/nextbike-live.xml?domains=mb"
+        handleResp resp =
+            case resp of
+                Http.Success data -> Just data
+                Http.Waiting -> Nothing
+                Http.Failure _ _ -> Nothing
+    in
+        handleResp <~ Http.sendGet (Signal.constant url)
