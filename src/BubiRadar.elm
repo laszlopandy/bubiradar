1800b1f71304215e9f0bb76b5cea2d9d655ac541
Laszlo Pandy <laszlo.pandy@prezi.com>
2014-12-25 15:02:41 +0100

    Station parsing finished with Result error handling.

--- src/BubiRadar.elm
+++ src/BubiRadar.elm
@@ -4,18 +4,63 @@ import Signal ((<~), (~), Signal)
 import Text (asText)
 import Http
 import Graphics.Element (flow, down)
+import String
+import Result
+import List
 
+{- Inward ports -}
 port userLocation : Signal (Maybe Location)
 port stationXmlIn : Signal (List StationXml) 
-
+{- Outward ports -}
 port stationXmlOut : Signal (Maybe String)
 port stationXmlOut = getBubiData
 
 port userLocationRequest : Signal ()
 port userLocationRequest = Signal.constant ()
 
+--resultMap2 : (a -> b -> c) -> Result e a -> Result e b -> Result e c
+--resultMap2 f a b = (Result.map f a) `Result.andThen` (\x -> Result.map x b)
 
-main = asText <~ ((,) <~ userLocation ~ stationXmlIn)
+(<~~) : (a -> b) -> Result e a -> Result e b
+(<~~) = Result.map
+(~~~) : Result e (a -> b) -> Result e a -> Result e b
+(~~~) f a = f `Result.andThen` (\x -> Result.map x a)
+
+
+makeLocation : String -> String -> Result String Location
+makeLocation lat lng =
+    Location
+        <~~ (String.toFloat lat)
+        ~~~ (String.toFloat lng)
+
+makeStation : StationXml -> Result String Station
+makeStation xml =
+    let makeRecord num_bikes max_bikes loc = {
+                uid = xml.uid,
+                location = loc,
+                unique_name = xml.unique_name,
+                name = xml.unique_name,
+                num_bikes = num_bikes,
+                max_bikes = max_bikes,
+                distance = Nothing
+            }
+    in
+        makeRecord
+            <~~ String.toInt xml.num_bikes
+            ~~~ String.toInt xml.max_bikes
+            ~~~ makeLocation xml.lat xml.lng
+
+stations : List StationXml -> List Station
+stations list =
+    List.filterMap (makeStation >> Result.toMaybe) list
+
+main =
+    Signal.map
+        asText
+        (Signal.map2
+            (,)
+            userLocation
+            (Signal.map stations stationXmlIn))
 
 type alias StationXml = {
         uid : String,
